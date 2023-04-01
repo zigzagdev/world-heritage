a548abb14d9e87c5f5a8eed7498f433a77a75ad9
zigzagdev <msts.oo0131@gmail.com>
2023-04-01 19:33:35 +0900

    darkMode_create

---
+++ app/src/components/Toogle.tsx
@@ -0,0 +1,188 @@
+import {useState} from "react";
+import "@fontsource/roboto";
+import {
+    Switch,
+    Container,
+    CssBaseline,
+    createTheme,
+    PaletteMode,
+    ThemeProvider
+} from "@mui/material";
+
+const Toggle = () => {
+    const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+    const changeTheme = () => {
+        setIsDarkTheme(!isDarkTheme);
+    };
+    const light = {
+        palette: {
+            mode: "light" as PaletteMode,
+        },
+    };
+
+    const dark = {
+        palette: {
+            mode: "dark" as PaletteMode
+        },
+    };
+
+    return (
+        <ThemeProvider theme={isDarkTheme ? createTheme(dark) : createTheme(light)}>
+            <CssBaseline/>
+            <Container>
+                <div style={{marginLeft: '700px'}}>
+                <Switch checked={isDarkTheme} onChange={changeTheme}/>
+                </div>
+            </Container>
+        </ThemeProvider>
+    );
+};
+
+export default Toggle;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
