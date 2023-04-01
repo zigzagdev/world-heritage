361bd2aef7fa0bd401d65cd82793e458efed6dc5
zigzagdev <msts.oo0131@gmail.com>
2023-04-01 13:11:41 +0900

    fixHeader

--- app/src/layouts/Header.tsx
+++ app/src/layouts/Header.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <div　style={{backgroundColor: "skyblue", padding: '40px'}}>
-            <h1 style={{padding: '5px 18px'}}>
+        <div　style={{backgroundColor: "skyblue", padding: '30px'}}>
+            <h1 style={{padding: '5px 15px'}}>
                 WorldHeritage
             </h1>
         </div>
