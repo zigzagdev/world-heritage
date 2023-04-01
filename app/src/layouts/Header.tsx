7307802a55d285fa3664d49a662c597e0f217672
zigzagdev <msts.oo0131@gmail.com>
2023-04-01 14:51:01 +0900

    fixDarkMode

--- app/src/layouts/Header.tsx
+++ app/src/layouts/Header.tsx
@@ -1,13 +1,62 @@
-import React from 'react';
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import { useTheme, ThemeProvider, createTheme } from '@mui/material/styles';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import Brightness7Icon from '@mui/icons-material/Brightness7';
 
-const Header = () => {
+const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+function Header() {
+    const theme = useTheme();
+    const colorMode = React.useContext(ColorModeContext);
     return (
-        <div　style={{backgroundColor: "skyblue", padding: '30px'}}>
-            <h1 style={{padding: '5px 15px'}}>
-                WorldHeritage
-            </h1>
-        </div>
+        <Box
+            sx={{
+                display: 'flex',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                bgcolor: 'background.default',
+                color: 'text.primary',
+                borderRadius: 1,
+                p: 3,
+            }}
+        >
+            {theme.palette.mode} mode
+            <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+                {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
+        </Box>
     );
-};
+}
 
-export default Header;
+export default function ToggleColorMode() {
+    const [mode, setMode] = React.useState<'light' | 'dark'>('light');
+    const colorMode = React.useMemo(
+        () => ({
+            toggleColorMode: () => {
+                setMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+            },
+        }),
+        [],
+    );
+
+    const theme = React.useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode,
+                },
+            }),
+        [mode],
+    );
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <Header />
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
+}
