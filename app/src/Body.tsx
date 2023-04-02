d67a7a6aef1247038378b955abe7a3064376f7f8
zigzagdev <msts.oo0131@gmail.com>
2023-04-02 14:15:20 +0900

    temporaryBody_making

---
+++ app/src/Body.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <div style={{paddingTop: "40px"}}>
+            <a href="/" style={{textDecoration: "none"}}>
+                <strong>World Heritage</strong>
+            </a>
+            <br/>
+            <a href="/" style={{textDecoration: "none"}}>
+                <strong>World Heritage</strong>
+            </a>
+            <br/>
+            <a href="/" style={{textDecoration: "none"}}>
+                <strong>World Heritage</strong>
+            </a>
+            <br/>
+            <a href="/" style={{textDecoration: "none"}}>
+                <strong>World Heritage</strong>
+            </a>
+            <br/>
+            <a href="/" style={{textDecoration: "none"}}>
+                <strong>World Heritage</strong>
+            </a>
+            <br/>
+            <a href="/" style={{textDecoration: "none"}}>
+                <strong>World Heritage</strong>
+            </a>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <a href="/" style={{textDecoration: "none"}}>
+                <strong>World Heritage</strong>
+            </a>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <a href="/" style={{textDecoration: "none"}}>
+                <strong>World Heritage</strong>
+            </a>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <a href="/" style={{textDecoration: "none"}}>
+                <strong>World Heritage</strong>
+            </a>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <a href="/" style={{textDecoration: "none"}}>
+                <strong>World Heritage</strong>
+            </a>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        </div>
+    );
+};
+
+export default Body;
