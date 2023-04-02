59e09cd675ec3477b09f03bb4f3c3c84e048f6d8
zigzagdev <msts.oo0131@gmail.com>
2023-04-02 18:11:51 +0900

    TopPageResponsive_fix

---
+++ app/src/components/Top.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import MediaQuery from "react-responsive";
+
+const Top = () => {
+    return (
+        <div>
+            <MediaQuery query="(max-width: 767px)">
+
+            </MediaQuery>
+            <MediaQuery query="(min-width: 768px)">
+
+            </MediaQuery>
+        </div>
+    )
+}
+
+export default Top;
