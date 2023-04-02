b0de05a192b73299d0bd6bd390efc10b80cce1fd
zigzagdev <msts.oo0131@gmail.com>
2023-04-02 11:53:42 +0900

    testingMainComponent

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -2,11 +2,73 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+        <a href="/" style={{textDecoration: "none"}}>
+            <strong>World Heritage</strong>
+        </a>
+        <br/>
+        <a href="/" style={{textDecoration: "none"}}>
+            <strong>World Heritage</strong>
+        </a>
+        <br/>
+        <a href="/" style={{textDecoration: "none"}}>
+            <strong>World Heritage</strong>
+        </a>
+        <br/>
+        <a href="/" style={{textDecoration: "none"}}>
+            <strong>World Heritage</strong>
+        </a>
+        <br/>
+        <a href="/" style={{textDecoration: "none"}}>
+            <strong>World Heritage</strong>
+        </a>
+        <br/>
+        <a href="/" style={{textDecoration: "none"}}>
+            <strong>World Heritage</strong>
+        </a>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <a href="/" style={{textDecoration: "none"}}>
+            <strong>World Heritage</strong>
+        </a>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <a href="/" style={{textDecoration: "none"}}>
+            <strong>World Heritage</strong>
+        </a>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <a href="/" style={{textDecoration: "none"}}>
+            <strong>World Heritage</strong>
+        </a>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <a href="/" style={{textDecoration: "none"}}>
+            <strong>World Heritage</strong>
+        </a>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+      <Footer/>
     </div>
   );
 }
