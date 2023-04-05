46e8ee2c0529217048be01e238c8ab0772e5c831
zigzagdev <msts.oo0131@gmail.com>
2023-04-05 21:57:22 +0900

    fixWidthBrowser

--- app/src/layouts/Header.tsx
+++ app/src/layouts/Header.tsx
@@ -4,37 +4,9 @@ import Theme from "../components/Toogle";
 import MediaQuery from "react-responsive";
 
 const outline = {
-    display: 'flex',
-}
-
-const right2 = {
-    display: "block",
-    margin: '30px 10px'
-};
-
-const headerSame = {
     display: "flex",
-    marginLeft: "40px",
-};
-
-const maxHeaderSame = {
-    display: "flex",
-    marginLeft: "700px",
-};
-
-const tabHead = {
-    display: "flex",
-    marginLeft: "100px"
-};
-
-const sameLine = {
-    display: "flex",
-    margin: '30px 10px'
-}
-
-const tabLine = {
-    display: "flex",
-    margin: "30px 0"
+    margin: "30px 20px",
+    justifyContent: "space-between"
 }
 
 const title = {
@@ -59,11 +31,11 @@ const Header = () => {
     return (
         <div>
             <MediaQuery query="(max-width: 640px)">
-                <div style={{display: "flex", margin: "30px 20px", justifyContent: "space-between"}}>
+                <div style={outline}>
                     <div>
                         <div>
                             <a href="/" style={{textDecoration: "none"}}>
-                                <strong style={font}>World</strong>
+                                <strong style={{fontSize: '20px'}}>World Heritage</strong>
                             </a>
                         </div>
                     </div>
@@ -107,7 +79,7 @@ const Header = () => {
                 </div>
             </MediaQuery>
             <MediaQuery query="(min-width: 1025px)">
-                <div style={{display: "flex", margin: "30px 20px", justifyContent: "space-between"}}>
+                <div style={{display: "flex", margin: "30px 100px", justifyContent: "space-between"}}>
                     <div>
                         <div>
                             <a href="/" style={{textDecoration: "none"}}>
