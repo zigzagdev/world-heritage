45e9ce7c9591a3e09dbafa1dd23c02b792c3ab67
zigzagdev <msts.oo0131@gmail.com>
2023-04-07 01:51:46 +0900

    footerIndet_fix

--- app/src/layouts/Footer.tsx
+++ app/src/layouts/Footer.tsx
@@ -12,41 +12,85 @@ const Footer = () => {
         <div>
             <MediaQuery query="(max-width: 640px)">
                 <div style={outline}>
-                    <div>
-                        test1
-                    </div>
-                    <div>
-                        test2
+                    <div style={{paddingBottom: '8%',}}>
+                        <div style={{display: 'inline', padding: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>Search</strong>
+                            </a>
+                        </div>
+                        <div style={{display: 'inline', margin: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>Top</strong>
+                            </a>
+                        </div>
+                        <div style={{display: 'inline', margin: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>SNS</strong>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </MediaQuery>
             <MediaQuery query="(min-width: 641px) and (max-width: 768px)">
                 <div style={outline}>
-                    <div>
-                        test1
-                    </div>
-                    <div>
-                        test2
+                    <div style={{paddingBottom: '8%',}}>
+                        <div style={{display: 'inline', padding: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>Search</strong>
+                            </a>
+                        </div>
+                        <div style={{display: 'inline', margin: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>Top</strong>
+                            </a>
+                        </div>
+                        <div style={{display: 'inline', margin: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>SNS</strong>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </MediaQuery>
             <MediaQuery query="(min-width: 769px) and (max-width: 1024px)">
                 <div style={outline}>
-                    <div>
-                        test1
-                    </div>
-                    <div>
-                        test2
+                    <div style={{paddingBottom: '8%',}}>
+                        <div style={{display: 'inline', padding: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>Search</strong>
+                            </a>
+                        </div>
+                        <div style={{display: 'inline', margin: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>Top</strong>
+                            </a>
+                        </div>
+                        <div style={{display: 'inline', margin: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>SNS</strong>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </MediaQuery>
             <MediaQuery query="(min-width: 1025px)">
                 <div style={outline}>
-                    <div>
-                        test1
-                    </div>
-                    <div>
-                        test2
+                    <div style={{paddingBottom: '8%',}}>
+                        <div style={{display: 'inline', padding: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>Search</strong>
+                            </a>
+                        </div>
+                        <div style={{display: 'inline', margin: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>Top</strong>
+                            </a>
+                        </div>
+                        <div style={{display: 'inline', margin: '0 7%'}}>
+                            <a href="/" style={{textDecoration: 'none'}}>
+                                <strong style={{fontSize: '30px'}}>SNS</strong>
+                            </a>
+                        </div>
                     </div>
                 </div>
             </MediaQuery>
