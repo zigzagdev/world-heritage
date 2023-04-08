dcb0e4df7e2780c9dc0b3255b25b94be0c93057a
zigzagdev <msts.oo0131@gmail.com>
2023-04-08 14:35:04 +0900

    fixPath_packageJson

--- app/src/components/Top.tsx
+++ app/src/components/Top.tsx
@@ -4,12 +4,7 @@ import MediaQuery from "react-responsive";
 const Top = () => {
     return (
         <div>
-            <MediaQuery query="(max-width: 767px)">
-
-            </MediaQuery>
-            <MediaQuery query="(min-width: 768px)">
-
-            </MediaQuery>
+            test here
         </div>
     )
 }
