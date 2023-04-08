f2c6a5088f6ce3df00d0fc56729b5e2fe3186464
zigzagdev <msts.oo0131@gmail.com>
2023-04-08 12:32:17 +0900

    bodyFileFix

--- app/src/Body.tsx
+++ app/src/Body.tsx
@@ -1,55 +1,18 @@
 import React from 'react';
 import MediaQuery from "react-responsive";
-import Theme from "./components/Toogle";
-
-const outline = {
-    display: 'flex',
-}
-
-const right2 = {
-    display: "block",
-    margin: '30px 10px'
-};
-
-const headerSame = {
-    display: "flex",
-    margin: "0 40px 0 auto",
-};
-
-const tabHead = {
-    display: "flex",
-    marginLeft: "20px"
-};
-
-const sameLine = {
-    display: "flex",
-    margin: '30px 10px'
-}
-
-const tabLine = {
-    display: "flex",
-    margin: "30px 0"
-}
-
-const title = {
-    margin: "0 auto 0 35px",
-}
-
-const tabTitle = {
-    margin: "0 auto 0 10px",
-}
-
-const font = {
-    fontSize: "30px",
-    color: "gray",
-}
 
 const Body = () => {
     return (
         <div style={{paddingTop: "40px"}}>
             <MediaQuery query="(max-width: 640px)">
                 <div>
-                    <p style={{textAlign: "center"}}>hhhhhhhhh</p>
+                    <a href="/" style={{textDecoration: "none"}}>
+                        <strong>World Heritage</strong>
+                    </a>
+                    <br/>
+                    <a href="/" style={{textDecoration: "none"}}>
+                        <strong>World Heritage</strong>
+                    </a>
                     <br/>
                     <br/>
                     <br/>
@@ -57,9 +20,15 @@ const Body = () => {
                     <br/>
                 </div>
             </MediaQuery>
-            <MediaQuery query="(min-width: 641px) and (max-width: 768)">
+            <MediaQuery query="(min-width: 641px) and (max-width: 768px)">
                 <div>
-                    <p style={{textAlign: "center"}}>hhhhhhhhh</p>
+                    <a href="/" style={{textDecoration: "none"}}>
+                        <strong>World Heritage</strong>
+                    </a>
+                    <br/>
+                    <a href="/" style={{textDecoration: "none"}}>
+                        <strong>World Heritage</strong>
+                    </a>
                     <br/>
                     <br/>
                     <br/>
@@ -69,7 +38,13 @@ const Body = () => {
             </MediaQuery>
             <MediaQuery query="(min-width: 769px) and (max-width: 1024px)">
                 <div>
-                    <p style={{textAlign: "center"}}>hhhhhhhhh</p>
+                    <a href="/" style={{textDecoration: "none"}}>
+                        <strong>World Heritage</strong>
+                    </a>
+                    <br/>
+                    <a href="/" style={{textDecoration: "none"}}>
+                        <strong>World Heritage</strong>
+                    </a>
                     <br/>
                     <br/>
                     <br/>
@@ -79,7 +54,13 @@ const Body = () => {
             </MediaQuery>
             <MediaQuery query="(min-width: 1025px)">
                 <div>
-                    <p style={{textAlign: "center"}}>hhhhhhhhh</p>
+                    <a href="/" style={{textDecoration: "none"}}>
+                        <strong>World Heritage</strong>
+                    </a>
+                    <br/>
+                    <a href="/" style={{textDecoration: "none"}}>
+                        <strong>World Heritage</strong>
+                    </a>
                     <br/>
                     <br/>
                     <br/>
@@ -92,3 +73,61 @@ const Body = () => {
 };
 
 export default Body;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
