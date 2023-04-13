cf12f4d7f2e6701317d24ee3664e7bdf8c74269f
zigzagdev <msts.oo0131@gmail.com>
2023-04-14 01:51:24 +0900

    fixTailWindConfig

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,9 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    xxs: {max: '428'},
+    sp: '428px', // スマホサイズを追加
+    maxsp: { max: '427px' }, // maxも追加
+    sptb: { min: '428px', max: '559px' }, // min-maxを追加
+
+    tb: '600px', // タブレットサイズを追加
+    maxtb: { max: '599px' },
+    tbpc: { min: '600px', max: '1023px' },
+
+    maxpc: { max: '1023px' },
+    pc: '1024px',
+
     extend: {},
   },
   plugins: [],
