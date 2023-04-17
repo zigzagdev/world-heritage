33d34969b178d6973d1e5c5336d8f685a42ba3a9
zigzagdev <msts.oo0131@gmail.com>
2023-04-17 22:08:36 +0900

    fixFooter

--- app/src/layouts/Footer.tsx
+++ app/src/layouts/Footer.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-
+        <div className="justify-end flex xxs:justify-center">
+            <div className="xxs:flex-col item-center">
+            <div className='text-yellow-500'>
+                test
+            </div>
+            <div className='text-yellow-500'>
+                test
+            </div>
+            <div className='text-green-700'>
+                test
+            </div>
+            </div>
         </div>
     )
 }
