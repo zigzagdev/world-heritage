8a1e2365f175cd97a62ffaec0d8733190df511f1
zigzagdev <msts.oo0131@gmail.com>
2023-04-23 18:03:02 +0900

    cardComponentAdd

--- app/src/components/Top.tsx
+++ app/src/components/Top.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import {Card} from "@mui/material";
+
 
 const Top = () => {
     return (
         <>
-            <div className='pb-5 px-5'>
+            <div className='pb-5 px-8'>
                 <strong>Popular Places</strong>
-
+                <div className='py-10 flex px-3 justify-between flex-wrap'>
+                    <Card style={{width: '200px', height: '100px'}} className='mx-2 my-5'>
+                        Conan
+                    </Card>
+                    <Card style={{width: '200px', height: '100px'}} className='mx-3 my-5'>
+                        Conan
+                    </Card>
+                    <Card style={{width: '200px', height: '100px'}} className='mx-3 my-5'>
+                        Conan
+                    </Card>
+                    <Card style={{width: '200px', height: '100px'}} className='mx-3 my-5'>
+                        Conan
+                    </Card>
+                    <Card style={{width: '200px', height: '100px'}} className='mx-3 my-5'>
+                        Conan
+                    </Card>
+                    <Card style={{width: '200px', height: '100px'}} className='mx-3 my-5'>
+                        Conan
+                    </Card>
+                </div>
             </div>
         </>
     );
