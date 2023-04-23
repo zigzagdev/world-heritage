46307362d93cc5ad16f5751af54ab8d5a85d9a48
zigzagdev <msts.oo0131@gmail.com>
2023-04-23 14:15:24 +0900

    fixIndentConfigFlie

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,21 +1,21 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    screens: {
-      'xxs': {'min': '1px', 'max': '367px'},
-      'xs': {'min': '368px', 'max': '639px'},
-      'sm': {'min': '640px', 'max': '767px'},
-      'md': {'min': '768px', 'max': '1023px'},
-      'lg': {'min': '1024px', 'max': '1279px'},
-      'xl': {'min': '1280px', 'max': '1535px'},
-      '2xl': {'min': '1536px'},
-    },extend: {
-      'xxs': {'min': '1px', 'max': '367px'},
-      'xs': {'min': '368px', 'max': '639px'},
-    }
-  },
-  plugins: [],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+        screens: {
+            'xxs': {'min': '1px', 'max': '367px'},
+            'xs': {'min': '368px', 'max': '639px'},
+            'sm': {'min': '640px', 'max': '767px'},
+            'md': {'min': '768px', 'max': '1023px'},
+            'lg': {'min': '1024px', 'max': '1279px'},
+            'xl': {'min': '1280px', 'max': '1535px'},
+            '2xl': {'min': '1536px'},
+        }, extend: {
+            'xxs': {'min': '1px', 'max': '367px'},
+            'xs': {'min': '368px', 'max': '639px'},
+        }
+    },
+    plugins: [],
 }
 
