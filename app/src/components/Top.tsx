f37556758ce2014a0773b8e4213f4d2538c30b50
zigzagdev <msts.oo0131@gmail.com>
2023-04-24 22:34:07 +0900

    baseCardCreated

--- app/src/components/Top.tsx
+++ app/src/components/Top.tsx
@@ -10,31 +10,36 @@ import {
 const Top = () => {
     return (
         <>
-            <div className='max-w-[1400px] mx-10 md:mx-5 xs:mx-8'>
+            <div className='max-w-[1400px] mx-10 mb-8 md:mx-5 sm:mx-5 xs:mx-8 xxs:mx-8'>
                 <strong>Popular Places</strong>
                 <div className='my-5'>
                     <div className="
                      grid grid-cols-3 sm:grid-cols-2 md:grid-cols-3
-                    sm:gap-4 gap-y-4 sm:gap-y-8 xs:grid-cols-1  xs:justify-items-center xxs:grid-cols-1 xxs:justify-items-center">
-                        <div className="px-3">
-                            <Card style={{background:'red', height: '100px', width: '240px'}}>Fuji</Card>
+                     sm:gap-4 gap-y-4 sm:gap-y-8 xs:grid-cols-1  xs:justify-items-center xxs:grid-cols-1
+                     xxs:justify-items-center"
+                    >
+                        <div className="mx-3.5">
+                            <Card style={{background: 'red', height: '100px', width: '240px'}}>Fuji</Card>
                         </div>
-                        <div className="mx-3">
-                            <Card style={{background:'red', height: '100px', width: '240px'}}>Fuji</Card>
+                        <div className="mx-3.5">
+                            <Card style={{background: 'red', height: '100px', width: '240px'}}>Fuji</Card>
                         </div>
-                        <div className="mx-3">
-                            <Card style={{background:'red', height: '100px', width: '240px'}}>Fuji</Card>
+                        <div className="mx-3.5">
+                            <Card style={{background: 'red', height: '100px', width: '240px'}}>Fuji</Card>
                         </div>
-                        <div className="mx-3">
-                            <Card style={{background:'red', height: '100px', width: '240px'}}>Fuji</Card>
+                        <div className="mx-3.5">
+                            <Card style={{background: 'red', height: '100px', width: '240px'}}>Fuji</Card>
                         </div>
-                        <div className="mx-3">
-                            <Card style={{background:'red', height: '100px', width: '240px'}}>Fuji</Card>
+                        <div className="mx-3.5">
+                            <Card style={{background: 'red', height: '100px', width: '240px'}}>Fuji</Card>
                         </div>
-                        <div className="mx-3">
-                            <Card style={{background:'red', height: '100px', width: '240px'}}>Fuji</Card>
+                        <div className="mx-3.5">
+                            <Card style={{background: 'red', height: '100px', width: '240px'}}>Fuji</Card>
                         </div>
                     </div>
+                </div>
+                <div>
+
                 </div>
             </div>
         </>
