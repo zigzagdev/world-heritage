f8b36e87c67c94126e53e03e9617bafe670a0847
zigzagdev <msts.oo0131@gmail.com>
2023-04-27 01:43:34 +0900

    asiaComponentFix

--- app/src/components/continents/Asia.tsx
+++ app/src/components/continents/Asia.tsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import {Card} from "@mui/material";
 
 function Asia() {
     return (
-        <div>
-
+        <div className='mx-16 xs:mx-5 xxs:mx-3'>
+            <div className='my-9 mx-24 xs:mx-5 xxs:mx-3'>
+                <div className='xxs:items-center xs:items-center'>
+                    Top popular Heritage in Asia Rank
+                </div>
+                <div className='xxs:items-center xs:items-center my-0 mx-auto xxs:my-4 xs:my-6 flex flex-wrap '>
+                    <div className="mx-4 my-3">
+                        <Card style={{background: 'red', height: '200px', width: '330px'}}>Fuji</Card>
+                    </div>
+                    <div className="mx-4 my-3">
+                        <Card style={{background: 'red', height: '200px', width: '330px'}}>Fuji</Card>
+                    </div>
+                    <div className="mx-4 my-3">
+                        <Card style={{background: 'red', height: '200px', width: '330px'}}>Fuji</Card>
+                    </div>
+                    <div className="mx-4 my-3">
+                        <Card style={{background: 'red', height: '200px', width: '330px'}}>Fuji</Card>
+                    </div>
+                    <div className="mx-4 my-3">
+                        <Card style={{background: 'red', height: '200px', width: '330px'}}>Fuji</Card>
+                    </div>
+                    <div className="mx-4 my-3">
+                        <Card style={{background: 'red', height: '200px', width: '330px'}}>Fuji</Card>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
