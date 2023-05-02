6d54a0bbc4d61c83329381116094bb6e880b6169
zigzagdev <msts.oo0131@gmail.com>
2023-05-02 21:01:43 +0900

    SouthAmericaComponent_created

---
+++ app/src/components/continents/SouthAmerica.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PostCard from "../items/PostCard";
+
+type SouthAmericaHeritage = {
+    id: number;
+    name: string;
+    category: number;
+    explanation: string;
+}
+
+
+function SouthAmerica() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default SouthAmerica;
