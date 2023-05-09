1489cc9ed303fd1f0cb1deed3bff2a5efec32d29
zigzagdev <msts.oo0131@gmail.com>
2023-05-09 20:25:09 +0900

    changeApp

--- app/src/components/items/Toast.tsx
+++ app/src/components/items/Toast.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
-const Toast = () => {
+export interface ToastProps {
+    id: string;
+    destroy: () => void;
+    title: string;
+    content: string;
+    duration?: number;
+}
+
+const Toast: React.FC<ToastProps> = (props) => {
+    const { destroy, content, title, duration = 0, id } = props;
+
+    useEffect(() => {
+        if (!duration) return;
+
+        const timer = setTimeout(() => {
+            destroy();
+        }, duration);
+
+        return () => clearTimeout(timer);
+    }, [destroy, duration]);
+
     return (
         <div>
-
+            <div className={"toast-header"}>
+                <div>{title} {id}</div>
+                <button onClick={destroy}>X</button>
+            </div>
+            <div className={"toast-body"}>{content}</div>
         </div>
     );
 };
